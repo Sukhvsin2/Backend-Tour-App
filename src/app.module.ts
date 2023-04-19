d22e233175ae650fcbdcfd1d798fe10aa906f9c1
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-04-19 18:09:35 -0400

    locations module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LocationsModule } from './locations/locations.module';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true}), 
-  AuthModule, MongooseModule.forRoot(process.env.DB_URL)],
+  AuthModule, MongooseModule.forRoot(process.env.DB_URL), LocationsModule],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
