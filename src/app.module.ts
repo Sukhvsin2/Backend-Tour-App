c7a2201a5ccd1c7af33ed63c713ccbff4a63ca6a
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-04-19 14:11:03 -0400

    mongo module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongoModule } from './mongo/mongo.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MongoModule],
   controllers: [AppController],
   providers: [AppService],
 })
