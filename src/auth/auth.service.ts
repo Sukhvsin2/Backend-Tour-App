6d9a0ac0782c7de874388ff6d7d2a276dd6e9e6c
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2023-04-19 14:08:44 -0400

    requests working

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    signup(body: any){
+        return body;
+    }
+
+    login(body: any){
+        return body;
+    }
+}
